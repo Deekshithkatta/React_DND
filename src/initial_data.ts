e628f750f6d702a97e27a09c2cfaaddaea33ba8a
Deekshithkatta <dxtkatta@gmail.com>
2020-05-27 22:42:27 +0530

    Initial data for DragAndDrop

---
+++ src/initial_data.ts
@@ -0,0 +1,14 @@
+export const initialData= {
+    tasks: {
+        'task-1': {id:'task-1', content:'Code'},
+        'task-2': {id:'task-1', content:'Code'},
+        'task-3': {id:'task-1', content:'Code'}
+    },
+    columns: {
+        'column-1': {
+            id:'column-1',
+            title:'To Do',
+            taskIds: ['task-1','task-2','task-3']
+        }
+    }
+}
